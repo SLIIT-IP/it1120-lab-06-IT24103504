92072ea4d0f6bfb5aae74e902753016d8d3acdc3
IT24103504 <it24103504@my.sliit.lk>
2024-08-28 11:26:17 +0530

    Add IT24103504Lab6Q2A.java to Q2A

---
+++ Q2/IT24103504Lab6Q2A.java
@@ -0,0 +1,12 @@
+public class IT24103504Lab6Q2A{
+  public static void main(String []args){
+  
+int counter=1;
+
+while(counter<=10)
+{
+   System.out.print(counter+" ");
+   counter=counter+1;
+ }  
+}
+}
